16c3d3c0d008edc4607bc495e24b3c1ad8f0f0dc
sanford <xinjun091@gmail.com>
2013-11-12 23:16:29 +0800

    fix https request bug

--- exchange.js
+++ exchange.js
@@ -1,24 +1,16 @@
 //https://api.weibo.com/2/users/show.json?access_token=#{@access_token}&uid=#{uid}"
-var http = require('http');
-
+var http = require('https');
 var token = "2.003k6msBdANkCD0d72ec0623dEXb9E";
 var uid = "2718443117";
-
-var opt = {
-  host: 'api.weibo.com',
-  port: 80,
-  path: 'https://api.weibo.com/2/users/show.json?access_token='+token+'&uid='+uid
-};
-
-
+var url = 'https://api.weibo.com/2/users/show.json?access_token='+token+'&uid='+uid;
 //以下是接受数据的代码
 var body = '';
-var req = http.request(opt, function(res) {
+var req = http.get(url, function(res) {
   console.log("Got response: " + res.statusCode);
   res.on('data',function(chunk){
   body += chunk;
  }).on('end', function(){
-
+//在这里处理逻辑
   console.log(res.headers)
   console.log(body)
  });
