fda71dc05e0d0061cce7b1705de5ea928736bb18
sanford <xinjun091@gmail.com>
2013-11-25 23:31:05 +0800

    任意两个关键字组合统计

---
+++ key_partner.js
@@ -0,0 +1,103 @@
+/*
+任意两个关键词的 cookie数统计
+  
+*/
+var output = './doc/un_key_cookie';
+var key_path="./cookie/cookie_keywords.data";
+//1CH03SJp00BppvQaybW1  更换机油,汽油添加剂,行车记录仪
+//process.argv[2] == undefined?"./cookie/cookie_keywords.data":process.argv[2];
+
+var fs = require('fs');
+var keys = {};
+var rs = fs.createReadStream(key_path, {
+    flags: 'r',
+    encoding: 'utf-8'
+});
+
+var ws = fs.createWriteStream(output, {
+    flags: 'w+',//a：追加，w+ :复写
+    encoding: 'utf-8'
+});
+
+var matched = false;
+var CRLF = '\n';
+var prevline = '';
+
+var timeIn = Date.parse(new Date());
+var cLines =0;
+var ctkey = 0;
+
+
+function unique(key) {
+ 
+  if(keys[key] == undefined){
+
+    keys[key] = 1;
+    ctkey++;
+
+  }else{
+    keys[key]++ ;
+  }
+
+}
+
+rs.on('data', function (chunk) {
+
+   var lines = chunk.split(CRLF);
+
+    lines[0] = prevline + lines[0];
+    if (chunk.substr(chunk.length - 2) !== CRLF) {
+        prevline = lines.pop();
+    } else {
+        prevline = '';
+    }
+
+  cLines+=lines.length;
+ console.log("已读取行数为："+cLines+"  已耗时："+((Date.parse(new Date())-timeIn)/1000)+"秒"+CRLF);
+
+    lines.forEach(function (line) {
+
+     var tmp = line.split("\t"); 
+     var stk = tmp[1].split(",");
+     var len = stk.length;
+        if(stk!==undefined&&len>=2){
+           stk.sort();
+           for (var i = 0;i<len;i++) {
+              for (var j = i+1;j<len;j++) {
+               unique(stk[i]+" "+stk[j]);
+              };
+          }
+        }
+    });
+
+
+});
+rs.on('end', function () {
+   // ws.write(keyword+"总数："+count + CRLF);
+    ws.write('[un_keywords_count :'+ctkey+"]"+ CRLF);
+    var term = [];
+    for(v in keys){
+      term.push(v);
+    };
+
+    term.sort(function(a,b){
+      return keys[a]- keys[b];
+    });
+
+    for (var i = term.length - 1; i >= 0; i--) {
+      ws.write("['"+term[i]+"':"+keys[term[i]]+"]"+CRLF);
+    };
+
+
+    ws.end(function () {
+
+      console.log('数据跑步完成！共收集任意两个关键字组合'+ctkey+'个');
+
+      for (var i = term.length - 1; i >= 0; i--) {
+
+      console.log("任意两个关键字组合： '"+term[i]+" '的统计量为： "+keys[term[i]]+CRLF);
+      };
+    
+      
+    });
+}); 
