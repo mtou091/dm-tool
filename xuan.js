11c8b6b6121c6b3d81965c5161fb628c940c9c73
sanford <xinjun091@gmail.com>
2013-11-29 12:09:49 +0800

    区域过滤

---
+++ xuan.js
@@ -0,0 +1,65 @@
+/*分地域过滤cookie
+*/
+var output = './doc/gpk.data';
+var key_path="./doc/cookie_info.data";
+//1CH00K0VD1BMN1scYKeH  广东  深圳
+//process.argv[2] == undefined?"./cookie/cookie_keywords.data":process.argv[2];
+
+var fs = require('fs');
+var keys = ["浙江","江苏","山东","上海","安徽"];
+var rs = fs.createReadStream(key_path, {
+    flags: 'r',
+    encoding: 'utf-8'
+});
+
+var ws = fs.createWriteStream(output, {
+    flags: 'w+',//a：追加，w+ :复写
+    encoding: 'utf-8'
+});
+
+var matched = false;
+var CRLF = '\n';
+var prevline = '';
+
+var timeIn = Date.parse(new Date());
+var cLines =0;
+var ctkey = 0;
+
+
+rs.on('data', function (chunk) {
+
+   var lines = chunk.split(CRLF);
+
+    lines[0] = prevline + lines[0];
+    if (chunk.substr(chunk.length - 2) !== CRLF) {
+        prevline = lines.pop();
+    } else {
+        prevline = '';
+    }
+
+  cLines+=lines.length;
+ console.log("已读取行数为："+cLines+"  已耗时："+((Date.parse(new Date())-timeIn)/1000)+"秒"+CRLF);
+
+    lines.forEach(function (line) {
+
+     for (var i = keys.length - 1; i >= 0; i--) {
+        if(line.indexOf(keys[i])!= -1){
+          console.log(keys[i]+CRLF);
+          ws.write(line+CRLF);
+          ctkey++;
+          break;
+        }
+     };
+
+    });
+
+
+});
+rs.on('end', function () {
+   console.log('数据跑步完成！收集:'+ctkey+"行"+CRLF);
+    ws.end(function () {
+
+      console.log('数据跑步完成！');
+      
+    });
+}); 
